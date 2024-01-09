1863308c46fb126294f8a5489973547a239fad28
Amaury Miguel Caballero <62359438+LarryKapija@users.noreply.github.com>
2024-01-09 01:49:25 -0400

    test(view): testTokenizerViewControllerUI function

--- TokenizerApp/TokenizerAppUITests/TokenizerAppUITests.swift
+++ TokenizerApp/TokenizerAppUITests/TokenizerAppUITests.swift
@@ -9,25 +9,21 @@ import XCTest
 
 final class TokenizerAppUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testTokenizerViewControllerUI() {
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let textField = app.textFields["SentenceTextField"]
+        let tokenizeButton = app.buttons["TokenizeButton"]
+        let sentencesLabel = app.staticTexts["SentencesLabel"]
+        
+        textField.tap()
+        textField.typeText("Hi my name is Oxus and I am here to start the project")
+        tokenizeButton.tap()
+        
+        // Verify that sentencesLabel updates correctly
+        XCTAssertTrue(sentencesLabel.label.contains("Hi my name is Oxus"))
+        XCTAssertTrue(sentencesLabel.label.contains("I am here to start the project"))
     }
 
     func testLaunchPerformance() throws {
