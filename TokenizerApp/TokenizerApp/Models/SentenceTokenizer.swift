13838f77485cfc9dc292b77dd2942395fc93cf2d
Amaury Miguel Caballero <62359438+LarryKapija@users.noreply.github.com>
2024-01-09 22:24:03 -0400

    fix(model): fixing logic in order to avoid bugs

--- TokenizerApp/TokenizerApp/Models/SentenceTokenizer.swift
+++ TokenizerApp/TokenizerApp/Models/SentenceTokenizer.swift
@@ -14,23 +14,38 @@ enum TokenizerError: Error {
 class SentenceTokenizer {
     private let tokens: [String: [String]]
     
-    init(tokens: [String : [String]] = ["en": ["if", "and"], "es": ["si", "y"]]) {
+    init(tokens: [String : [String]] = ["en": ["if","and"], "es": ["si","y"]]) {
         self.tokens = tokens
     }
     
     func tokenize(sentence: String, language: String = "en") throws -> [String] {
-        
-        guard let keywords = tokens[language] else {
+        guard let keywords = tokens[language]?.map({ $0.lowercased() }) else {
             throw TokenizerError.unsupportedLanguage
         }
+
+        if sentence.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            return []
+        }
+
+        var sentences = [String]()
+        var currentSentence = ""
         
-        var sentences = [sentence]
-        for keyword in keywords {
-            sentences = sentences
-                .flatMap { $0.components(separatedBy: " \(keyword) ") }
-                .map { $0.trimmingCharacters(in: .whitespaces) }
+        let words = sentence.split(separator: " ", omittingEmptySubsequences: false)
+        for word in words {
+            if keywords.contains(word.lowercased()) {
+                if !currentSentence.isEmpty {
+                    sentences.append("- \(currentSentence.trimmingCharacters(in: .whitespaces))")
+                }
+                currentSentence = "\(word) "
+            } else {
+                currentSentence += "\(word) "
+            }
+        }
+
+        if !currentSentence.isEmpty {
+            sentences.append("- \(currentSentence.trimmingCharacters(in: .whitespaces))")
         }
         
-        return sentences.filter { !$0.isEmpty }
+        return sentences
     }
 }
